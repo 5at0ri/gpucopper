b5961527494489a4c8f95df22f4738c952b71d1b
satori <165195527+5at0ri@users.noreply.github.com>
2025-01-30 16:37:06 +0000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,16 +1,13 @@
 /** @type {import("next").NextConfig} */
 const config = {
   reactStrictMode: true,
-
   images: {
     domains: ["storage.ko-fi.com"],
   },
-
   i18n: {
     locales: ["en"],
     defaultLocale: "en",
   },
-
   output: "standalone", // ✅ Ensures proper deployment on Vercel
 };
 
